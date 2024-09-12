7708a4797f430aa437276580697aec901a77e9db
luciaecheverry <echeverryferreyralucia@gmail.com>
2024-09-12 09:05:50 -0300

    Delete Desafío LocalStorage directory

--- Desafío LocalStorage/js/index.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("buttonText").addEventListener("click", function() {
-    const inputText = document.getElementById("inputText").value;
-    // Guardamos el valor en el localStorage bajo la clave "dato"
-    localStorage.setItem("dato", inputText);
-});
